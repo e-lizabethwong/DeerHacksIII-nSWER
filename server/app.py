c86beb6c5851dcb34c13ebb122ecd585b75eebf8
Anne <anne12080901@gmail.com>
2024-02-17 11:13:46 -0500

    edited the routes

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from flask import Flask
+from flask import Flask, request, redirect
 
 url = "https://api.openai.com/v1/chat/completions"
 
@@ -25,11 +25,21 @@ data = {
 
 
 app = Flask(__name__)
-@app.route('/submit', methods=['POST'])
+
+@app.route('/')
+def index():
+    return redirect('/submit')
+
+@app.route('/submit', methods=['GET', 'POST'])
 def prompt():
-    response = requests.post(url, headers=headers, data=json.dumps(data))
+    if request.method == 'POST':
+        response = request.post(url, headers=headers, data=json.dumps(data))
     # print(response.text)
-    return(json.loads(response.text)['choices'][0]['message']["content"])
+        return (json.loads(response.text)['choices'][0]['message']["content"])
+    else: 
+        return 'worked'
+#    elif request.method == 'GET':
+#        return 'hi'
 
 if __name__ == '__main__':
     app.run(debug=True)
